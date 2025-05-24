a75059a48bda6541c14c2395562f80d1a6ed553a
Djdjdallas <dominickjerell@gmail.com>
2025-05-24 10:31:12 -0700

    careers

--- .history/src/app/page_20250524103028.jsx
+++ .history/src/app/page_20250524103028.jsx
@@ -800,6 +800,7 @@ export default function HomePage() {
                       About
                     </Link>
                   </li>
+
                   <li>
                     <Link
                       href="#"
